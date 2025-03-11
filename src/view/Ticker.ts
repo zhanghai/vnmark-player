bc37bcabf9009a489af8359938673ee5df2df721
Hai Zhang <dreaming.in.code.zh@gmail.com>
2025-03-10 19:33:48 -0700

    Feat: Auto pause and resume ticker depending on whether there's callback
    CPU usage can drop to 0 when there's no transition thanks to this.

--- src/view/Ticker.ts
+++ src/view/Ticker.ts
@@ -9,8 +9,58 @@ export class Ticker {
   private readonly callbacks = new Map<unknown, TickerCallback>();
 
   start() {
+    if (this.isStarted) {
+      return;
+    }
     this.lastTimestamp = document.timeline.currentTime as DOMHighResTimeStamp;
+    if (this.callbacks.size) {
+      this.resume();
+    }
+  }
+
+  stop() {
+    if (!this.isStarted) {
+      return;
+    }
+    this.pause();
+    this.lastTimestamp = undefined;
+  }
+
+  get isStarted(): boolean {
+    return this.lastTimestamp !== undefined;
+  }
+
+  pause() {
+    if (!this.isResumed) {
+      return;
+    }
+    cancelAnimationFrame(this.requestId!);
+    this.requestId = undefined;
+  }
+
+  resume() {
+    if (this.isResumed) {
+      return;
+    }
     this.requestId = requestAnimationFrame(it => this.update(it));
+  }
+
+  get isResumed(): boolean {
+    return this.requestId !== undefined;
+  }
+
+  addCallback(callbackId: unknown, callback: TickerCallback) {
+    this.callbacks.set(callbackId, callback);
+    if (this.callbacks.size) {
+      this.resume();
+    }
+  }
+
+  removeCallback(callbackId: unknown) {
+    this.callbacks.delete(callbackId);
+    if (!this.callbacks.size) {
+      this.pause();
+    }
   }
 
   private update(timestamp: DOMHighResTimeStamp) {
@@ -18,24 +68,5 @@ export class Ticker {
     this.lastTimestamp = timestamp;
     this.requestId = requestAnimationFrame(it => this.update(it));
     this.callbacks.forEach(it => it(timestamp));
-  }
-
-  stop() {
-    if (this.requestId !== undefined) {
-      cancelAnimationFrame(this.requestId);
-    }
-    this.lastTimestamp = undefined;
-  }
-
-  get isRunning(): boolean {
-    return this.requestId !== undefined;
-  }
-
-  addCallback(callbackId: unknown, callback: TickerCallback) {
-    this.callbacks.set(callbackId, callback);
-  }
-
-  removeCallback(callbackId: unknown) {
-    this.callbacks.delete(callbackId);
   }
 }
